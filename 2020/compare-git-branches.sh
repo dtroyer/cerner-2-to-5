c88257ebbddfa86f0b9ea1fa2e413e4b94aebaf3
Dean Troyer <dtroyer@gmail.com>
2020-09-16 14:03:57 -0500

    Day 3: compare-git-branches.sh - git log wrapper to find unique commits
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

---
+++ 2020/compare-git-branches.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Compare two git branches
+# $0 [--left|--right] <repo> [<repo> ...]
+#
+# <repo> is a path to a git repo
+#
+# Show the commits unique to either or both branches
+# Defaults to master and HEAD on the current branch
+
+# Defaults
+LEFT_BRANCH=${LEFT_BRANCH:-master}
+RIGHT_BRANCH=${RIGHT_BRANCH:-HEAD}
+
+WORK_DIR=$(pwd)
+
+if [[ -n $1 ]]; then
+    if [[ "$1" == "--left" ]]; then
+        GIT_DIFF="--left-only"
+        shift
+    elif [[ "$1" == "--right" ]]; then
+        GIT_DIFF="--right-only"
+        shift
+    else
+        GIT_DIFF="--left-right"
+    fi
+fi
+
+GIT_ARGS="$GIT_DIFF --oneline --cherry-pick --no-merges"
+
+for i in $@; do
+    (
+        cd $repo; \
+        git log ${GIT_ARGS} ${LEFT_BRANCH} ${RIGHT_BRANCH}; \
+    )
+done
